c42c220defae0273e14ed6a5eda77b69dc92c8e2
rjphayes <hayesreider@gmail.com>
2014-10-07 11:20:35 -0400

    wrote more of two power

--- bigTwoPower.pl
+++ bigTwoPower.pl
@@ -1,5 +1,8 @@
 use Math::BigInt;
 my $binString = '0b1'.'0'x1000;
 my $numH = Math::BigInt->new($binString);
-print $numH,"\n";
-
+$sum = 0;
+while ($x->digit($numH)){
+	$sum += $x;
+}
+print "$sum\n";
