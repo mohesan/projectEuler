fea4b1d801e4415b1cce241f68aeb1492f3cdac9
rjphayes <reid.hayes@sickkids.ca>
2014-10-20 09:47:51 -0400

    syncing

--- play.pl
+++ play.pl
@@ -1,12 +1,1 @@
-# #for my $i (0..0){
-# #	print $i,"\n";
-# #}
-# @list = (0,1,2);
-# print $#list;
-
-# for $i (0..10){
-# 	print $i;
-# }
-
-my $a = 128;
-print !($a &($a-1));
+print Y eq X,"\n";
